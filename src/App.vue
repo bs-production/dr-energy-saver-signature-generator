ef23679cb63371fa26a20220c3037fbce073a982
chrisroselli <chrisroselli@gmail.com>
2020-05-06 12:08:51 -0400

    brand update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <div class="hero-body">
         <div class="container">
           <div class="th-logo has-text-centered">
-            <img src="https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/321/DES-We-Fix-Logo.png" alt="" style="width:280px">
+            <img src="https://dc69b531ebf7a086ce97-290115cc0d6de62a29c33db202ae565c.ssl.cf1.rackcdn.com/325/lj-des-we-fix-logo.png" alt="" style="width:280px">
           </div>
         <h1 class="sub-text title has-text-centered">
             <p>Signature Generator</p> 
@@ -76,7 +76,7 @@
         <div class="container">
             <div class="content has-text-centered">
                 <p>
-                    &copy;2020 Dr. Energy Saver
+                    &copy;2020 Connecticut Dr. Energy Saver
                 </p>
             </div>
         </div>
